5f460198fce7f9e5d6dc729ac4f590814894592a
Marlette Dakiwas <mjessdakiwas@gmail.com>
2025-10-23 19:26:43 -0400

    Connected to Perplexity and successfully fetched responses based on mock queries

--- server/index.js
+++ server/index.js
@@ -2,19 +2,21 @@
 fulfill requirement for refactoring code to Node.js
 already installed npm: express, cors, dotenv -JL */
 
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 
 /* test app - run node server.js in terminal to check if it runs
 update: it works/runs! may need second pair of eyes to confirm -JL */
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/api/health", (req, res)=> res.json({ok:true}));
+app.get('/api/health', (req, res) => res.json({ ok: true }));
 
 //placeholder route
-app.post('/api/quiz', (req, res) => res.json({ message: "Perplexity integration pending" }));
+app.post('/api/quiz', (req, res) =>
+  res.json({ message: 'Perplexity integration pending' })
+);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, ()=> console.log(`Running on server ${PORT}`));
+app.listen(PORT, () => console.log(`Running on server ${PORT}`));
