83b531241c1c5d956807cb21f234f4911d1943ad
Jane Le <janehtle@gmail.com>
2025-10-23 00:05:49 -0400

    initial upload of client and server

---
+++ server/index.js
@@ -0,0 +1,6 @@
+//server folder will be for Node + Express
+//fulfill requirement for refactoring code to Node.js
+//already installed npm: express, cors, dotenv
+
+const express = require("express");
+const cors = require("cors");
